6d98dc38e4d31604504457214072172bd100d796
sarria <416509194@qq.com>
2021-09-25 11:56:11 +0800

    add home tab row and scroll page

--- app/src/main/java/com/sarria/fake_shanbay_compose/MainActivity.kt
+++ app/src/main/java/com/sarria/fake_shanbay_compose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sarria.fake_shanbay_compose
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,13 +15,22 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.sarria.fake_shanbay_compose.ui.Main
+import com.sarria.fake_shanbay_compose.ui.home.Home
+import com.sarria.fake_shanbay_compose.ui.login.Login
 import com.sarria.fake_shanbay_compose.ui.splash.Splash
 import com.sarria.fake_shanbay_compose.ui.theme.Fake_shanBay_composeTheme
 
+
+val Context.datastore :DataStore<Preferences> by preferencesDataStore(name = "login")
 class MainActivity : ComponentActivity() {
 
+    @ExperimentalPagerApi
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,19 +47,28 @@ class MainActivity : ComponentActivity() {
 
 
 //是闪屏页还是主调用流程
+@ExperimentalPagerApi
 @ExperimentalAnimationApi
 @Composable
 fun ShanBayApp() {
     var splashed by rememberSaveable {
         mutableStateOf(false)
     }
+
+    var login by rememberSaveable {
+        mutableStateOf(false)
+    }
+
     Surface(modifier = Modifier.fillMaxSize()) {
-        Crossfade(splashed) {
-            if (it) {
-                Main()
-            } else {
-                Splash { splashed = true }
+        if (!login){
+            Login{
+                login = true
+                splashed = true
             }
+        } else if (!splashed){
+            Splash { splashed =true }
+        } else{
+            Home()
         }
     }
 }
